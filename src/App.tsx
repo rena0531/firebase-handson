8010f0eec6939ef64f90aa291897b35948071d86
Rena <choco.rena0609@gmail.com>
2020-06-18 21:48:45 +0900

    allowSyntheticDefaultImports追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 interface Comment {
   id: number;
@@ -13,75 +13,7 @@ interface Form {
 }
 
 const App: React.FC = () => {
-  const [comments, setComments] = useState([]);
-  const [form, setForm] = useState({ user: "", body: "" });
-
-  const handleSubmitForm = (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    const { comments, form } = this.state;
-    comments.unshift({
-      id: uuid(),
-      time: moment().format("YYYY/MM/DD HH:mm:ss"),
-      ...form,
-    });
-    form.body = "";
-    this.setState({
-      comments,
-      form,
-    });
-  };
-
-  const updateForm = (key: string, value: string) => {
-    
-    const { form } = this.state;
-    form[key] = value;
-    this.setState({
-      form,
-    });
-  };
-
-  return (
-    <>
-      <div className="container">
-        <h1>React Application</h1>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="four columns">
-            <form onSubmit={handleSubmitForm}>
-              <p>
-                <label>User ID</label>
-                <input
-                  type="text"
-                  onInput={(event) => updateForm("user", event.target.value)}
-                  value={form.user}
-                />
-              </p>
-              <p>
-                <label>Body</label>
-                <textarea
-                  onInput={(event) => updateForm("body", event.target.value)}
-                  value={form.body}
-                />
-              </p>
-              <button className="button button-primary">投稿</button>
-            </form>
-          </div>
-          <div className="eight columns">
-            <ul>
-              {comments.map((comment: Comment) => (
-                <li key={comment.id}>
-                  <strong>{comment.user}</strong>
-                  <p>{comment.body}</p>
-                  <p style={{ textAlign: "right" }}>{comment.time}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <>Hello</>;
 };
 
 export default App;
