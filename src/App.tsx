eca83e9017c9b9cca724577e5f9dc280aee4c404
Rena <choco.rena0609@gmail.com>
2020-06-22 08:27:31 +0900

    5. ログインに応じた処理の出し分けの実装

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ interface Comment {
 }
 
 interface Form {
-  user: string;
+  user: string | string[] | number;
   body: string;
 }
 
@@ -20,6 +20,9 @@ const initialFormState = { user: "", body: "" };
 const App: React.FC = () => {
   const [comments, setComments] = useState<Comment[]>([]);
   const [form, setForm] = useState<Form>(initialFormState);
+  const [isLoadedUser, setIsLoadedUser] = useState(false);
+  const [isConnectedToDatabase, setIsConnectedToDatabase] = useState(false);
+  const [user, setUser] = useState<string | null>("");
 
   const handleClickLogin = () => {
     firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
@@ -27,16 +30,17 @@ const App: React.FC = () => {
 
   const handleSubmitForm = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const id = `${moment().unix()}${uuidv4().substr(0, 9).replace("-", "")}`;
     const payload = {
-      id: `${moment().unix()}${uuidv4().substr(0, 9).replace("-", "")}`,
+      id: id,
       time: moment().format("YYYY/MM/DD HH:mm:ss"),
       user: form.user,
       body: form.body,
     };
     //setComments((comments) => [payload, ...comments]);
-    database.ref(`comments/${payload.id}`).set(payload);
-    console.log(payload);
-    setForm({ user: "", body: "" });
+    database.ref(`comments/${id}/${user}`).set(payload);
+    //修正
+    setForm({ user: payload.user, body: "" });
   };
 
   const handleChange = (
@@ -47,20 +51,34 @@ const App: React.FC = () => {
     setForm(newForm);
   };
 
+  const handleClickLogout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        location.href = location.href;
+      });
+  };
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        alert(user.email + " でログインしています");
-      } else {
-        alert("このユーザーはログインしていません");
+        setUser(user.email);
+        setForm({ user: user.email, body: "" });
       }
+      setIsLoadedUser(true);
     });
 
     database.ref("comments/").on("value", (snapshot) => {
       const comments = Object.values(snapshot.val() || {}) || [];
       setComments(comments);
+      setIsConnectedToDatabase(true);
     });
   }, []);
+
+  if (!isLoadedUser || !isConnectedToDatabase) {
+    return <div className="loading">Loading...</div>;
+  }
 
   return (
     <>
@@ -71,46 +89,62 @@ const App: React.FC = () => {
       <div className="container">
         <div className="row">
           <div className="four columns">
-            <p>
-              <button
-                type="button"
-                onClick={handleClickLogin}
-                className="button button-primary"
-                style={{ width: "100%" }}
-              >
-                Google アカウントでログイン
-              </button>
-            </p>
-            <form onSubmit={handleSubmitForm}>
+            {user ? (
+              <form onSubmit={handleSubmitForm}>
+                <p>
+                  <label>User ID</label>
+                  <input
+                    readOnly
+                    type="text"
+                    name="user"
+                    onChange={handleChange}
+                    value={form.user}
+                  />
+                </p>
+                <p>
+                  <label>Body</label>
+                  <textarea
+                    name="body"
+                    onChange={handleChange}
+                    value={form.body}
+                  />
+                </p>
+                <button className="button button-primary">投稿</button>
+                <hr />
+                <button
+                  type="button"
+                  onClick={handleClickLogout}
+                  className="button button-default"
+                >
+                  ログアウト
+                </button>
+              </form>
+            ) : (
               <p>
-                <label>User ID</label>
-                <input
-                  type="text"
-                  name="user"
-                  onChange={handleChange}
-                  value={form.user}
-                />
+                <button
+                  type="button"
+                  onClick={handleClickLogin}
+                  className="button button-primary"
+                  style={{ width: "100%" }}
+                >
+                  Google アカウントでログイン
+                </button>
               </p>
-              <p>
-                <label>Body</label>
-                <textarea
-                  name="body"
-                  onChange={handleChange}
-                  value={form.body}
-                />
-              </p>
-              <button className="button button-primary">投稿</button>
-            </form>
+            )}
           </div>
           <div className="eight columns">
             <ul>
-              {comments.map((comment: Comment) => (
-                <li key={comment.id}>
-                  <strong>{comment.user}</strong>
-                  <p>{comment.body}</p>
-                  <p style={{ textAlign: "right" }}>{comment.time}</p>
-                </li>
-              ))}
+              {comments.map((commentData, i) => {
+                const comment = Object.values(commentData || {})[0];
+                if (!comment) return null;
+                return (
+                  <li key={i}>
+                    <strong>{comment.user}</strong>
+                    <p>{comment.body}</p>
+                    <p style={{ textAlign: "right" }}>{comment.time}</p>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
