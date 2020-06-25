2c921bc360b2100b4fc4100d8ba97e16a1f12e32
Rena <choco.rena0609@gmail.com>
2020-06-25 17:26:26 +0900

    7.設定の変更

--- functions/src/utils/firebase.ts
+++ functions/src/utils/firebase.ts
@@ -2,8 +2,10 @@ import { config } from "../../config";
 import * as firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth";
+import "firebase/storage";
 
 firebase.initializeApp(config);
 const database = firebase.database();
+const storage = firebase.storage();
 
-export { firebase, database };
+export { firebase, database, storage };
